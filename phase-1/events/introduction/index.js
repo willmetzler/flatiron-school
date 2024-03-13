272e940013977582b00eb077447cc0a235178531
Will Metzler <metzlerwill@gmail.com>
2024-03-13 16:24:50 -0400

    update notes

--- phase-1/events/introduction/index.js
+++ phase-1/events/introduction/index.js
@@ -2,11 +2,12 @@
 
 Phase 1 -> DOM Events
 by Sakib Rasul
-Updated on September 17, 2023 | Created on September 13, 2023
+Updated March 13, 2024
+Created September 13, 2023
 
 Objectives
-1. Do something when to #tomorrow when it is clicked.
-2. Do something to #sometime when #form is submitted.
+1. Do something when an event fires on an element.
+2. Handle a form submission by reading its input.
 
 Takeaways
 - "click" and "submit" events
@@ -15,31 +16,14 @@ Takeaways
 
 */
 
-
-// Let's add a click event listener to #tomorrow. First, we'll need a reference.
-const tomorrowTag = document.querySelector("#tomorrow");
-// Next, we add a `click` listener to `tomorrowTag`.
-tomorrowTag.addEventListener("click", () => {
-    // Whatever we write here will run when a user clicks on "Tomorrow."
-    console.log("The user clicked on Tomorrow.");
-    tomorrowTag.textContent = "Tomorrow!";
-    tomorrowTag.remove();
+// ~ the "click" event
+// the target (what element the event will fire on, eg. #today), the event (the user action, "click"), and the handler (log)
+document.querySelector("#today").addEventListener("click", event => {
+    console.log("The user clicked on 'Today!")
 });
+//second parameter is what you want to happen, or callback function
 
-// Now let's write a form submission handler.
-const form = document.querySelector("#form");
-form.addEventListener("submit", (event) => {
-    // By default, browsers redirect to a new URL when a form is submitted.
-    // This means that our event handler doesn't get to run :(
-    // To prevent this (and any other default) behavior, we can use `event.preventDefault()`.
-    // `event` is an object that all event handlers have access to,
-    // and `preventDefault()` is an event method that suppresses default behavior.
-    event.preventDefault();
-    console.log("The user entered a date!");
-    // We can use dot or bracket notation to access a form's input elements.
-    // `form.date` is equivalent to `<input name="date" .../>`.
-    // `form.date.value` is the value entered into `<input name="date" .../>`.
-    const newDate = form.date.value;
-    // Now that we have the selected date, we can populate #sometime with it!
-    document.querySelector("#sometime").textContent = "On " + newDate + ".";
-});
+// ~ the "submit" event
+
+// ~ challenges
+// 1. Add some non-click, non-submit event listener to the page. It can trigger whatever you wish!
